4b6c8280a6fe5707940df1d33ec502ac14ed815b
Nicolas <nicolascarobene1@gmail.com>
2023-07-29 18:17:58 +0200

    rm Main.java clase Vehiculos

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
-    public static void main(String[] args) {
-       Coche yeyo = new Coche(4,180,120,2000,500);
-       furgoneta furgo = new furgoneta(4,180,120,2000,500,7,580);
-        System.out.println(furgo.toString());
-    }
-}
